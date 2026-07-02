81981a757a9a957799c0e4e4414d9a85c8439b3b
Chase Prengaman <chprengaman@gmail.com>
2026-07-02 12:56:43 -0400

    Remove Vercel build output from repo

--- .vercel/output/functions/__server.func/_ssr/cookbook._id-rTmhmcl3.mjs
+++
@@ -1,6 +0,0 @@
-import { m as createFileRoute, p as lazyRouteComponent } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/cookbook._id-rTmhmcl3.js
-var $$splitComponentImporter = () => import("./cookbook._id-iofBVBtW.mjs");
-var Route = createFileRoute("/cookbook/$id")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
--- .vercel/output/functions/__server.func/_ssr/drinks._id-DKPgw4Yz.mjs
+++
@@ -1,6 +0,0 @@
-import { m as createFileRoute, p as lazyRouteComponent } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/drinks._id-DKPgw4Yz.js
-var $$splitComponentImporter = () => import("./drinks._id-B0qN0jGB.mjs");
-var Route = createFileRoute("/drinks/$id")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
